d20e9269a61287f0e7caaa6f431144eaf5b5c45d
Johan van Amersfoort <49033774+vhojan@users.noreply.github.com>
2025-06-16 15:52:16 +0200

    New version

---
+++ templates/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request, render_template
+from amt_controller import power_control, check_amt
+from cluster_status import get_cluster_load
+
+app = Flask(__name__, static_url_path='/static', static_folder='static', template_folder='templates')
+
+NODES = {
+    "osmt-node1": {"ip": "192.168.5.11"},
+    "osmt-node2": {"ip": "192.168.5.12"},
+    "osmt-node3": {"ip": "192.168.5.13"},
+}
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/status")
+def status():
+    return jsonify({
+        "cluster_load": get_cluster_load(),
+        "amt": {node: check_amt(data["ip"]) for node, data in NODES.items()}
+    })
+
+@app.route("/power/<node>/<action>", methods=["POST"])
+def power(node, action):
+    if node not in NODES or action not in ["on", "off", "reset"]:
+        return jsonify({"error": "Invalid node or action"}), 400
+    return jsonify(power_control(NODES[node]["ip"], action))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
